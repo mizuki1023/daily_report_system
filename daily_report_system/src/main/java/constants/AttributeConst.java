a2e19176e4a1082a4e27e3faba4560948fa2b321
apple <apple@192.168.3.3>
2024-05-13 20:51:59 +0900

    日報showアクションの作成

--- daily_report_system/src/main/java/constants/AttributeConst.java
+++ daily_report_system/src/main/java/constants/AttributeConst.java
@@ -1,5 +1,11 @@
 package constants;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import actions.views.ReportView;
+
 public enum AttributeConst {
   //フラッシュメッセージ
     FLUSH("flush"),
@@ -66,4 +72,26 @@ public enum AttributeConst {
         return this.i;
     }
 
+    /**
+     * 詳細画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void show() throws ServletException, IOException {
+
+        //idを条件に日報データを取得する
+        ReportView rv = service.findOne(toNumber(getRequestParam(AttributeConst.REP_ID)));
+
+        if (rv == null) {
+            //該当の日報データが存在しない場合はエラー画面を表示
+            forward(ForwardConst.FW_ERR_UNKNOWN);
+
+        } else {
+
+            putRequestScope(AttributeConst.REPORT, rv); //取得した日報データ
+
+            //詳細画面を表示
+            forward(ForwardConst.FW_REP_SHOW);
+        }
+    }
 }
